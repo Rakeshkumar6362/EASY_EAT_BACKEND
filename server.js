67af8bb0425c7fb6cf90414a80001613127e8178
Rakeshkumar6362 <rakeshkumarrocky77@gmail.com>
2021-12-28 14:55:57 +0530

    updated server

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-// app.use(router)
+app.use(router)
 // app.get("/", (req, res) => {
 //   res.send("hell0");
 // });
