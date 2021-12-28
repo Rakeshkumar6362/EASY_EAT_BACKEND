da04e3fd8499cc67a48c33cbe20ae603cc76ce3b
Rakeshkumar6362 <rakeshkumarrocky77@gmail.com>
2021-12-28 14:24:23 +0530

    changed server

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 
 import cors from "cors";
-import router from "./Routes/router";
+// import router from "./Routes/router";
+// import router from './Routes/router'
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
