d94286e7be44787eee77b3847ca0cd7be920174a
Rakeshkumar6362 <rakeshkumarrocky77@gmail.com>
2021-12-28 14:57:51 +0530

    updated url in server

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use(router)
+app.use("/EASY_EAT",router)
 // app.get("/", (req, res) => {
 //   res.send("hell0");
 // });
