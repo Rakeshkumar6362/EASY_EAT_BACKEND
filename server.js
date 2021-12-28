d83764299b8911b919fb2129d464792a7c2156a0
Rakeshkumar6362 <rakeshkumarrocky77@gmail.com>
2021-12-28 13:08:25 +0530

    Update connection changes

--- server.js
+++ server.js
@@ -8,17 +8,21 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors())
+app.use(cors());
 // app.use('/EASY_EAT',router)
 app.get("/", (req, res) => {
   res.send("hell0");
 });
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 // const connection = async()=>{
-mongoose.connect(process.env.DATABASE, { useNewUrlParser: true });
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => {
+    app.listen(PORT, () => console.log(process.env.PORT));
+  })
+  .catch((err) => console.log("err"));
 // }
 // connection()
-mongoose.connection.on("connected", (err, res) => {
-  console.log("mongoose is connected");
-});
-app.listen(PORT, () => console.log(process.env.PORT));
+// mongoose.connection.on("connected", (err, res) => {
+//   console.log("mongoose is connected");
+// });
