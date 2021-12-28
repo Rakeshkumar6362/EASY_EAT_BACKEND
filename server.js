6a2f7e2d03a30117b5a6fde0f2c6f2480ea7c353
Rakeshkumar6362 <rakeshkumarrocky77@gmail.com>
2021-12-28 13:23:13 +0530

    updated server

--- server.js
+++ server.js
@@ -8,8 +8,10 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors());
-app.use('/EASY_EAT',router)
-
+// app.use('/EASY_EAT',router)
+app.get("/", (req, res) => {
+  res.send("hell0");
+});
 const PORT = process.env.PORT || 5000;
 // const connection = async()=>{
 mongoose
